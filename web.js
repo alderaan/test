ec3133db1311100854d3128c293e6b8f42edf3a3
David <jollyneuron@gmail.com>
2013-09-29 18:55:35 +0200

    hello

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
+
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+var readtobuf = fs.readFileSync('index.html');
+var buftostring = readtobuf.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buftostring);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
